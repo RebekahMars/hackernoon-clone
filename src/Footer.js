dd1f7717de0d3f18d34b4ebca3dd489d343f967f
RebekahMars <47405076+RebekahMars@users.noreply.github.com>
2021-05-17 21:04:13 -0700

    we be styling the footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ function Footer()
         <div className="footer-container">
             <div className="footer-left">
                 <div className="about-footer">
-                    <h2>ABOUT</h2>
-                    <ul>
+                    <h2 className="footer-title">ABOUT</h2>
+                    <ul className="footer-links">
                         <li>Careers</li>
                         <li>Contact</li>
                         <li>Cookies</li>
@@ -15,8 +15,8 @@ function Footer()
                     </ul>
                 </div>
                 <div className="read-footer">
-                    <h2>READ</h2>
-                    <ul>
+                    <h2 className="footer-title">READ</h2>
+                    <ul className="footer-links">
                         <li>Archive</li>
                         <li>Leaderboard</li>
                         <li>Signup</li>
@@ -26,8 +26,8 @@ function Footer()
                     </ul>
                 </div>
                 <div className="write-footer">
-                    <h2>WRITE</h2>
-                    <ul>
+                    <h2 className="footer-title">WRITE</h2>
+                    <ul className="footer-links">
                         <li>Distribution</li>
                         <li>Editor Tips</li>
                         <li>Guidelines</li>
@@ -37,8 +37,8 @@ function Footer()
                     </ul>
                 </div>
                 <div className="sponser-footer">
-                    <h2>SPONSER</h2>
-                    <ul>
+                    <h2 className="footer-title">SPONSER</h2>
+                    <ul className="footer-links">
                         <li>Brand Publishing</li>
                         <li>Case Studies</li>
                         <li>Niche Marketing</li>
@@ -50,11 +50,11 @@ function Footer()
             </div>
             <div className="footer-right">
                 <div className="newletter-footer">
-                    <h2>THE HACKER NOON NEWSLETTER</h2>
-                    <p>Quality Weekly Reads About Technology Infilitrating Everything</p>
+                    <h2 className="footer-title">THE HACKER NOON NEWSLETTER</h2>
+                    <p className="footer-text">Quality Weekly Reads About Technology Infilitrating Everything</p>
                     <div className="footer-search">
-                        <input type="search"></input>
-                        <button type="submit">Subscribe free</button>
+                        <input type="search" className="search" placeholder="name@company.com"></input>
+                        <button type="submit" className="footer-button">Subscribe free</button>
                     </div>
                     <input type="radio" className="subscribe"></input>
                     <label for="subscribe">Yes, I agree to receive emails about tech eating the world.</label>
