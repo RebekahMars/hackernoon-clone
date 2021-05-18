fa8e8298d253bab14eb319f8db7b4f5e26f305af
RebekahMars <47405076+RebekahMars@users.noreply.github.com>
2021-05-17 21:57:55 -0700

    styling

--- src/About-Section.js
+++ src/About-Section.js
@@ -3,15 +3,17 @@ function HackerAbout()
     return(
         <div className="about-section">
             <div className="tab-titles">
-                <ul>
-                    <li>For Readers</li>
-                    <li>For Writers</li>
-                    <li>For Sponsers</li>
-                    <li>About Us</li>
-                </ul>
+                <button className="about-button">For Readers</button>
+                <button className="about-button">For Writers</button>
+                <button className="about-button">For Sponsers</button>
+                <button className="about-button">About Us</button>
             </div>
             <div className="table-text-section">
-                <p></p>
+                <p className="tabletext">Hacker Noon reflects the technology industry with unfettered stories and opinions writeen by real tech professionals.
+                    We believe we can get closer to the truth by elevating thousands of voices. To the reader, we pldge no paywall, no 
+                    pop up ads, and evergreen (get it?) content. Learn how hackers start their afternoons on Hacker Noon.
+                </p>
+                <button type="submit" className="table-text-button"></button>
             </div>
         </div>
     )
